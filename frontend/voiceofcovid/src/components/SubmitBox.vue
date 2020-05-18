310471f92a740aee8ec42de759225ef6576f180d
Fernando Medina Corey <fmcorey@gmail.com>
2020-05-18 01:52:47 -0700

    Add messaging once upload works

--- frontend/voiceofcovid/src/components/SubmitBox.vue
+++ frontend/voiceofcovid/src/components/SubmitBox.vue
@@ -1,43 +1,44 @@
 <template>
-    <div>
-        <div class="submit-box" id="form-div">
-            <div class="ui brown top attached segment">
-                <h3>  Share your story:</h3>
-            </div>
-            <form class="ui equal width attached segment form" id="submission">
-                <div class="four fields">
-                    <div class="field">
-                    <label for="first_name">First name:</label>
-                    <input v-model="first_name" type="text" class="form-control" placeholder="Jasmine">
-                    </div>
-                    <div class="field">
-                    <label for="job">Job:</label>
-                    <input v-model="job" type="text" class="form-control" placeholder="Nurse">
-                    </div>
-                    <div class="field">
-                    <label for="description">Description:</label>
-                    <input v-model="description" type="text" class="form-control" placeholder="What's your story?">
-                    </div>
-                </div>
-                <div class="ui two column centered grid">
-                    <file-select v-model="file"></file-select>
-                </div>
-                <br>
-                <div class="ui two column centered grid">
-                    <div class="one column row">
-                    <div class="right floated item">
-                        <vue-recaptcha sitekey="6LfBV-YUAAAAAC2F4BdtYjZB72gCuP-aFRd0F_N7"></vue-recaptcha>
-                    </div>
-                    </div>
-                    <div class="one column row">
-                    <button v-on:click.prevent="submitForm()" type="submit" class="ui huge blue button">Submit</button>
-                    <div class="ui error message">
-                        <div id='error-message' style="color: red"></div>
-                    </div>
-                    </div>
-                </div>
-            </form>
+    <div class="submit-box">
+        <div v-show="!show_submit_box" class="ui brown top attached segment">
+            <h3>Thanks for sharing your story!</h3>
         </div>
+        <div v-show="show_submit_box" class="ui brown top attached segment">
+            <h3>  Share your story:</h3>
+        </div>
+        <form v-show="show_submit_box" class="ui equal width attached segment form" id="submission">
+            <div class="four fields">
+                <div class="field">
+                <label for="first_name">First name:</label>
+                <input v-model="first_name" type="text" class="form-control" placeholder="Jasmine">
+                </div>
+                <div class="field">
+                <label for="job">Job:</label>
+                <input v-model="job" type="text" class="form-control" placeholder="Nurse">
+                </div>
+                <div class="field">
+                <label for="description">Description:</label>
+                <input v-model="description" type="text" class="form-control" placeholder="What's your story?">
+                </div>
+            </div>
+            <div class="ui two column centered grid">
+                <file-select v-model="file"></file-select>
+            </div>
+            <br>
+            <div class="ui two column centered grid">
+                <div class="one column row">
+                <div class="right floated item">
+                    <vue-recaptcha sitekey="6LfBV-YUAAAAAC2F4BdtYjZB72gCuP-aFRd0F_N7"></vue-recaptcha>
+                </div>
+                </div>
+                <div class="one column row">
+                <button v-on:click.prevent="submitForm()" type="submit" class="ui huge blue button">Submit</button>
+                <div class="ui error message">
+                    <div id='error-message' style="color: red"></div>
+                </div>
+                </div>
+            </div>
+        </form>
     </div>
 </template>
 
@@ -78,6 +79,7 @@ export default {
                 endpointToPostAudioFile,
                 formData
             )
+            vm.show_submit_box = false
         })
         console.log("yup");
     }
@@ -88,6 +90,7 @@ export default {
         first_name: null,
         job: null,
         description: null,
+        show_submit_box: true,
     }
   },
 }
